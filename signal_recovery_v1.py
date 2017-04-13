f82a161f42af977e7037a00181dcae18a4ab6d8e
maggie0106 <yingxue.zhang2@mail.mcgill.ca>
2017-04-13 14:04:21 -0400

    17.04.13 update

--- signal_recovery_v1.py
+++ signal_recovery_v1.py
@@ -10,18 +10,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 #graph generation
-def graph_generation(N,graph_type,base_mode):
+def graph_generation(N,graph_type='ring graph',base_mode='adjacency'):
     if graph_type=='ring graph':
         k=4   #neighbor number
         p=0
         G=nx.newman_watts_strogatz_graph(N, k, p, seed=None)    #ring graph
-        fourier_basis=normalized_fourier_basis(G,base_mode)
+        fourier_basis=normalized_fourier_basis(G,base_mode='adjacency')
     if graph_type=='random graph':
         G = nx.erdos_renyi_graph(N,0.1)                         #random graph(Erdos-Renyi Graph)
-        fourier_basis=normalized_fourier_basis(G,base_mode)
-    if graph_type=='random geometric graph':
-        G=nx.random_geometric_graph(N,0.1)
-        fourier_basis=normalized_fourier_basis(G,base_mode)
+        fourier_basis=normalized_fourier_basis(G,base_mode='adjacency')
     return fourier_basis
 
 def matrix_eigenvalue_normalization(M):
@@ -37,16 +34,34 @@ def matrix_eigenvalue_normalization(M):
     Normalize_eigenvector = V_M[:,index_sig[::-1]]
     return  Normalize_eigenvector
     
+
+
 #fourier basis generation
-def normalized_fourier_basis(G,base_mode):
+def normalized_fourier_basis(G,base_mode='adjacency'):
     if base_mode=='adjacency':
         A=nx.adjacency_matrix(G, nodelist=None, weight='weight').todense() 
-        Fourier_basis=matrix_eigenvalue_normalization(A)
-        print 'adjacency mode'
+        Sigma_A, V_A = np.linalg.eigh(A) 
+        index_sig = np.argsort(Sigma_A)
+        Sigma_A = Sigma_A[index_sig[::-1]] #sorting the eigenvalue
+        V_A = V_A[:,index_sig[::-1]] #sorting the according eigenvectors
+        #Normalized matrix A
+        A=A/Sigma_A[0] #normalized matrix A
+        Sigma_A, V_A = np.linalg.eigh(A) 
+        index_sig = np.argsort(Sigma_A)
+        Sigma_A = Sigma_A[index_sig[::-1]]
+        Fourier_basis = V_A[:,index_sig[::-1]]
     if base_mode=='Laplacian':
         Laplacian = nx.normalized_laplacian_matrix(G).todense()
-        Fourier_basis=matrix_eigenvalue_normalization(Laplacian)
-        print 'Laplacian mode'
+        Sigma_L, V_L = np.linalg.eigh(Laplacian)
+        index_sig = np.argsort(Sigma_L)
+        Sigma_L = Sigma_L[index_sig[::-1]]
+        V_L = V_L[:,index_sig[::-1]]
+        #Normalized Laplacian matrix
+        Laplacian=Laplacian/Sigma_L[0] #normalized matrix A
+        Sigma_L, V_L = np.linalg.eigh(Laplacian) 
+        index_sig = np.argsort(Sigma_L)
+        Sigma_L = Sigma_L[index_sig[::-1]]
+        Fourier_basis = V_L[:,index_sig[::-1]]
     return Fourier_basis
     
 #signal generation
@@ -56,7 +71,7 @@ def graph_signal_generation(n,K,beta,Fourier_basis):
     signal=[]
     for i in range(N):
         if i<K:
-            signal.append(np.random.normal(1,0.5))
+            signal.append(np.random.normal(1,0.5*0.5))
         else:
             signal.append((float(K)/i)**(2*beta))
             #signal.append(0)  #test the completely band limited case
@@ -67,8 +82,7 @@ def graph_signal_generation(n,K,beta,Fourier_basis):
     signal_norm=signal_norm.reshape(len(signal),1)
     x=Fourier_basis*signal_norm
     x=np.asarray(x)
-    x=x.reshape(n,1)
-    x=np.matrix(x)
+    x=x.reshape(n,)
     return x
 #signal normalization
 def normalized(x):
@@ -80,16 +94,10 @@ def normalized(x):
 
 #generate the sampling score based on graph structure
 def sampling_score(N,Fourier_basis_K,sampling_mode):
-    if sampling_mode=='uniform_sampling':
+    if sampling_mode=='uniform sampling':
         dist=float(1)/N
         norm_dist=dist*np.ones(N)
-    if sampling_mode=='leverage_score':
-        dist=[]
-        for i in Fourier_basis_K:
-            dist.append(np.sqrt(np.linalg.norm(i)))
-        norm_dist=normalized(dist)
-        norm_dist=np.asarray(norm_dist)
-    if sampling_mode=='square_root_leverage_score':
+    if sampling_mode=='experiment design':
         dist=[]
         for i in Fourier_basis_K:
             dist.append(np.linalg.norm(i))
@@ -104,99 +112,70 @@ def sacle_matrix(sample_score,m):
         a=(float(1)/np.sqrt(m*i))
         Scaling_matrix.append(a)
     Scaling_matrix=np.asarray(Scaling_matrix) 
+    #Scaling_matrix=(float(1)/np.sqrt(m*distribution))*np.ones((N,)) #unidorm sampling
     Scaling_matrix=np.diag(Scaling_matrix)
     return Scaling_matrix
 
-def sample_operator(x,m,sample_score):
+def sample_operator(x,m):
 
     N=len(x)
     sampling_operator=np.zeros((m,N))
-    sample_index=np.random.choice(N,m, replace=False, p=sample_score)
+    sample_index=np.random.choice(N,m, replace=False, p=None)
     for i in range(m):
         sampling_operator[i][sample_index[i]]=1
     sampling_operator=np.matrix(sampling_operator)
     return sampling_operator
-
-def signal_recovery(signal_g,Fourier_basis,K,epsilon,sampling_mode):
-    Fourier_basis_K=Fourier_basis[:,0:K]
-    Inv_fourier_basis=np.linalg.inv(Fourier_basis)
-    Inv_fourier_basis_K=Inv_fourier_basis[0:K,:]
-    sample_score=sampling_score(N,Fourier_basis_K,sampling_mode)
-    Scaling_matrix=sacle_matrix(sample_score,m)
-    sampling_operator=sample_operator(signal_g,m,sample_score)
-    x1=signal_g.reshape(N,1)
-    x1=np.matrix(x1)
-    noise=np.random.normal(0,epsilon)
-    noise=np.ones((m,1))*noise
-    y=sampling_operator*x1+noise
-    x_recovery=Fourier_basis_K*Inv_fourier_basis_K*np.transpose(sampling_operator)\
-    *sampling_operator*Scaling_matrix*Scaling_matrix\
-    *np.transpose(sampling_operator)*y
-    return x_recovery
-
-def comparison_plot(N):
-    uniform_sampling_mse=pd.read_csv('MSE uniform_sampling.csv')
-    squre_root_leverage_mse=pd.read_csv('MSE square_root_leverage_score.csv')
-    leverage_score_mse=pd.read_csv('MSE leverage_score.csv')
-    
-    plt.plot(m_value,np.log(uniform_sampling_mse['MSE']),label='uniform sampling')
-    plt.plot(m_value,np.log(squre_root_leverage_mse['MSE']),label='square root leverage score')
-    plt.plot(m_value,np.log(leverage_score_mse['MSE']),label='leveragae score')
-    plt.legend(loc='upper right', shadow=True)
-    plt.title('log mean square error from different sampling method')
-    plt.xlabel('sample number')
-    plt.ylabel('log MSE')
-
 #================================================================================================
 #generate graph fourier transform basis
-N=1000 #node number in the network
+N=1000
 K=10 #band limit
-beta=1 #spectral decay factor
-epsilon=10**-4  #noise introducing into the system N~(0,epsilon)
-base_mode='adjacency'
-graph_type='random geometric graph'#'random geometric graph'    #'ring graph'
-
-Fourier_basis=graph_generation(N,graph_type,base_mode)
+beta=0.5 #control the frequency band outside the bandlimit
+Fourier_basis=graph_generation(N,graph_type='ring graph',base_mode='adjacency')
 Inv_fourier_basis=np.linalg.inv(Fourier_basis)
 signal_g=graph_signal_generation(N,K,beta,Fourier_basis)
 
-sampling_mode='uniform_sampling' #uniform_sampling or square_root_leverage_score
 MSE=[]
 Recovery_signal=[]
 
 m_value=range(100,N,100)
 for m in m_value:
     print 'm=',m
-    K_recovery=max(int(m**(float(1)/(2*beta+1))),20) 
-    K_recovery=10#recovery bandwidth
-    print K_recovery
-    x_recovery=signal_recovery(signal_g,Fourier_basis,K_recovery,epsilon,sampling_mode)
-    norm=np.linalg.norm(x_recovery-signal_g)
+    K=max(int(m**(float(1)/(2*beta+1))),10)
+    K=30
+    Fourier_basis_K=Fourier_basis[:,0:K]
+    Inv_fourier_basis_K=Inv_fourier_basis[0:K,:]
+    sample_score=sampling_score(N,Fourier_basis_K,sampling_mode='uniform sampling')
+    Scaling_matrix=sacle_matrix(sample_score,m)
+    
+    sampling_operator=sample_operator(signal_g,m)
+    x1=signal_g.reshape(N,1)
+    x1=np.matrix(x1)
+    SNR=np.random.normal(0,10**-4)
+    noise=np.ones((m,1))*SNR
+    y=sampling_operator*x1+noise
+    x_recovery=Fourier_basis_K*Inv_fourier_basis_K*np.transpose(sampling_operator)\
+    *sampling_operator*Scaling_matrix*Scaling_matrix\
+    *np.transpose(sampling_operator)*y
+    norm=np.linalg.norm(x_recovery-x1)
     print norm
     MSE.append(norm)
     Recovery_signal.append(x_recovery)
-#==================================Save file==============================================================   
-MSE1=pd.Series(MSE)
-file_name='MSE '+sampling_mode+'.csv'  
-MSE1.to_csv(file_name,index=False,header=['MSE'])
     
-#==================================Figures==============================================================  
-fig1=plt.figure(figsize=(9,6))
+fig1=plt.figure(1)
 plt.plot(signal_g,label='x original')
 plt.plot(x_recovery,label='x recovery')
 legend = plt.legend(loc='upper right', shadow=True)
 plt.title('comparison between recovery signal and original signal')
 
-fig2=plt.figure(figsize=(9,6))
+fig2=plt.figure(2)
 plt.plot(m_value,np.log(MSE))
-title='Log mean square error for '+sampling_mode
-plt.title(title)
+plt.title('Log mean square error')
 plt.xlabel('sample number')
 plt.ylabel('log MSE')
 
-#comparison_plot(N)
+MSE1=pd.Series(MSE)
+file_name='MSE_N_'+str(N)+'_beta_'+str(beta)
+MSE1.to_csv('MSE.csv')
 
-
-
-
-
+    
+#generate graph signal
